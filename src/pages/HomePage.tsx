cad8922ae8340f4fb81dff7ff26c2a87c1a38ca8
MuhammadAslam-K <aslamka.2k3@gmail.com>
2024-02-20 19:53:05 +0530

    completed the image carousel of the home section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,36 @@
+import ImageCarousel from "../components/Carousel/ImageCarousel"
 import Navbar from "../components/Navbar/Navbar"
 import Search from "../components/Search/Search"
 
 function HomePage() {
+
+    const images = [
+        '/img/hero-section-img1.jpg',
+        '/img/hero-section-img2.jpg',
+        '/img/hero-section-img3.jpg'
+    ]
     return (
         <div className="bg-white flex justify-center">
             <div className="w-10/12">
                 <Navbar />   {/* Navbar component */}
 
-                <div className="text-center my-10 mb-14 text-black font-zilla"> {/*  Intro Section */}
+                <section className="text-center my-10 mb-14 text-black font-zilla"> {/*  Intro Section */}
                     <h1 className="font-medium text-4xl my-5">The people platform—Where <br /> interests become friendships</h1>
                     <p>Whatever your interest, from hiking and reading to networking and skill sharing, <br /> there are thousands of people who share it on Meetup. Events are happening <br /> every day sign up to join the fun.</p>
+                </section>
+
+                <div className="my-10">
+                    <Search /> {/* Search input component */}
                 </div>
 
-                <Search /> {/* Search input component */}
+                <div className="my-5 mt-20">
+                    <ImageCarousel images={images} />  {/* ImageCarousel Component */}
+                </div>
+
+                <section className="text-center my-20 mb-14 text-black font-zilla">
+                    <h1 className="font-medium text-4xl my-5">The people platform—Where <br /> interests become friendships</h1>
+                </section>
+
             </div>
         </div>
 
